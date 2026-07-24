04702308fcad0cc4f2cfac41b22f25d155ebd8ae
Lukas Graf <graflukas218@gmail.com>
2026-07-24 11:15:07 +0200

    feat(style): add prettier

---
+++ prettier.config.ts
@@ -0,0 +1,19 @@
+import type { Options } from "prettier";
+
+const config: Options = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  insertPragma: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  endOfLine: "auto",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
+
+export default config;
