013e316a916aa959b0d22f9feea7a7b021289bc5
Lukas Manuel Graf <graflukas218@gmail.com>
2026-08-10 09:47:18 +0200

    refactor(logger): unfinished refactor. Goal: prettify CLI logging

--- dist/helpers/logger.js
+++ dist/helpers/logger.js
@@ -1,27 +1,86 @@
 import chalk from "chalk";
 import log from "loglevel";
+import { capitalize } from "./utils.js";
+function translateLogLevel(level) {
+  const map = [
+    "trace",
+    "debug",
+    "info",
+    "warn",
+    "error",
+    "silent"
+  ];
+  if (typeof level === "number" && 0 <= level && level <= 5) {
+    return map[level];
+  } else if (map.includes(level)) {
+    return level;
+  } else {
+    throw new TypeError("Invalid log level");
+  }
+}
+const hex = {
+  black: "#000",
+  white: "#fff",
+  error: "#e05353",
+  success: "#93c9cc",
+  warn: "#e8a917",
+  debug: "#6391cf",
+  brand: "#bb8354",
+  brandLight: "#fcfaf7"
+};
 class CliLogger {
-  static instance;
+  static colors = {
+    trace: chalk.white,
+    debug: chalk.bgMagenta.hex(hex.black).bold,
+    error: chalk.bgRed.hex(hex.black).bold,
+    warn: chalk.bgHex(hex.warn).hex(hex.black).bold,
+    info: chalk.bgHex(hex.success).hex(hex.black).bold,
+    default: chalk.cyan.hex(hex.black).bold,
+    scope: chalk.cyan,
+    var: chalk.cyan,
+    num: chalk.cyan
+  };
   logger;
-  _scope = "Scope";
-  constructor() {
+  static activeScope = "";
+  rawScope = "";
+  rawLabel = "";
+  rawLevel;
+  instanceColors = {};
+  inferScope;
+  _indentChar = "__indent__";
+  constructor(scope, inferScope = false) {
+    this.setScope(scope ?? "");
+    this.inferScope = inferScope;
     this.logger = log.getLogger("peakflow");
-    this.logger.setLevel("info");
+    this.rawLevel = translateLogLevel(this.logger.getLevel());
   }
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = new CliLogger();
-    }
-    return this.instance;
-  }
-  set scope(scope) {
-    this._scope = `[${scope}]`;
-  }
-  get scope() {
-    return this._scope;
+  replaceIndent(msg) {
+    return msg.map(
+      (part) => typeof part === "string" ? part.replace(this._indentChar, " ".repeat(this.rawLabel.length)) : part
+    );
   }
   get indent() {
-    return " ".repeat(this._scope.length);
+    return this._indentChar + " ".repeat(this.scope.length);
+  }
+  set colors(colors) {
+    this.instanceColors = { ...this.instanceColors, ...colors };
+  }
+  get colors() {
+    return {
+      ...CliLogger.colors,
+      ...this.instanceColors
+    };
+  }
+  get labelColor() {
+    const label = this.rawLevel;
+    const key = label in this.colors ? label : "default";
+    return this.colors[key];
+  }
+  get label() {
+    if (!this.rawLabel || this.rawLabel.toLowerCase() === "trace") {
+      return "";
+    }
+    return this.labelColor(` ${this.rawLabel} `) + " ";
   }
   get newLine() {
     return "\n";
@@ -29,8 +88,19 @@ class CliLogger {
   get nextLine() {
     return this.newLine + this.indent;
   }
+  get scope() {
+    if (this.inferScope) {
+      this.rawScope = CliLogger.activeScope;
+    } else if (this.rawScope) {
+      CliLogger.activeScope = this.rawScope;
+    } else {
+      return "";
+    }
+    return this.colors.scope(`[${this.rawScope}]`);
+  }
   setScope(scope) {
-    this.scope = scope;
+    this.rawScope = scope;
+    CliLogger.activeScope = this.rawScope;
   }
   setLevel(level) {
     this.logger.setLevel(level);
@@ -38,34 +108,48 @@ class CliLogger {
   getLevel() {
     return this.logger.getLevel();
   }
+  log(opts, ...msg) {
+    this.rawLevel = translateLogLevel(opts.level);
+    this.rawLabel = opts.label ?? capitalize(this.rawLevel);
+    if (this.rawLevel === "silent") return;
+    this.logger[this.rawLevel](this.label + this.scope, ...this.replaceIndent(msg));
+  }
   trace(...msg) {
-    this.logger.trace(chalk.cyan(this.scope), ...msg);
+    this.log({ level: "trace" }, ...msg);
   }
   debug(...msg) {
-    this.logger.debug(chalk.blue(this.scope), ...msg);
+    this.log({ level: "debug" }, ...msg);
   }
   info(...msg) {
-    this.logger.info(chalk.green(this.scope), ...msg);
+    this.log({ level: "info" }, ...msg);
   }
   warn(...msg) {
-    this.logger.warn(chalk.yellow(this.scope), ...msg);
+    this.log({ level: "warn" }, ...msg);
   }
   error(...msg) {
-    this.logger.error(chalk.red(this.scope), ...msg);
+    this.log({ level: "error" }, ...msg);
   }
   var(str) {
-    return chalk.cyan(str);
+    return this.colors.var(str);
   }
   num(num) {
-    return chalk.cyan(num);
+    return this.colors.num(num);
   }
   json(obj) {
     return JSON.stringify(obj, void 0, 4);
   }
 }
-const logger = CliLogger.getInstance();
+const logger = new CliLogger(void 0, true);
+const configLogger = new CliLogger("Config");
+const devLogger = new CliLogger("Dev");
+const buildLogger = new CliLogger("Build");
 var logger_default = logger;
 export {
   CliLogger,
-  logger_default as default
+  buildLogger,
+  configLogger,
+  logger_default as default,
+  devLogger,
+  hex,
+  logger
 };
--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,36 +1,108 @@
-import chalk from "chalk";
+import chalk, { ChalkInstance } from "chalk";
 import log, { type LogLevelNames } from "loglevel";
 import { PlainObject } from "../types/utils.js";
+import { capitalize } from "./utils.js";
 
 export type LogLevelNumber = 0 | 1 | 2 | 3 | 4 | 5;
+export type LogLevelName = LogLevelNames | "silent";
+
+function translateLogLevel(level: LogLevelName | LogLevelNumber): LogLevelName {
+  const map: LogLevelName[] = [
+    "trace",
+    "debug",
+    "info",
+    "warn",
+    "error",
+    "silent",
+  ];
+  if (typeof level === "number" && 0 <= level && level <= 5) {
+    return map[level]!;
+  } else if ((map as string[]).includes(level as string)) {
+    return level as LogLevelName;
+  } else {
+    throw new TypeError("Invalid log level");
+  }
+}
+
+export const hex = {
+  black: "#000",
+  white: "#fff",
+  error: "#e05353",
+  success: "#93c9cc",
+  warn: "#e8a917",
+  debug: "#6391cf",
+  brand: "#bb8354",
+  brandLight: "#fcfaf7",
+};
 
 export class CliLogger {
-  public static instance: CliLogger;
+  public static colors = {
+    trace: chalk.white,
+    debug: chalk.bgMagenta.hex(hex.black).bold,
+    error: chalk.bgRed.hex(hex.black).bold,
+    warn: chalk.bgHex(hex.warn).hex(hex.black).bold,
+    info: chalk.bgHex(hex.success).hex(hex.black).bold,
+    default: chalk.cyan.hex(hex.black).bold,
+    scope: chalk.cyan,
+    var: chalk.cyan,
+    num: chalk.cyan,
+  };
+
   public logger: log.Logger;
-  private _scope: string = "Scope";
 
-  private constructor() {
+  protected static activeScope: string = "";
+
+  private rawScope: string = "";
+  private rawLabel: string = "";
+  private rawLevel: LogLevelName;
+  private instanceColors: Record<string, ChalkInstance> = {};
+  private readonly inferScope: boolean;
+  private readonly _indentChar = "__indent__";
+
+  public constructor(scope?: string, inferScope = false) {
+    this.setScope(scope ?? "");
+    this.inferScope = inferScope;
     this.logger = log.getLogger("peakflow");
-    this.logger.setLevel("info");
+    this.rawLevel = translateLogLevel(this.logger.getLevel())
   }
 
-  public static getInstance() {
-    if (!this.instance) {
-      this.instance = new CliLogger();
-    }
-    return this.instance;
-  }
-
-  public set scope(scope: string) {
-    this._scope = `[${scope}]`;
-  }
-
-  public get scope(): string {
-    return this._scope;
+  private replaceIndent(msg: any[]) {
+    return msg.map((part) =>
+      typeof part === "string"
+        ? part.replace(this._indentChar, " ".repeat(this.rawLabel.length))
+        : part,
+    );
   }
 
   public get indent(): string {
-    return " ".repeat(this._scope.length);
+    return this._indentChar + " ".repeat(this.scope.length);
+  }
+
+  public set colors(colors: Record<string, ChalkInstance>) {
+    this.instanceColors = { ...this.instanceColors, ...colors };
+  }
+
+  public get colors(): typeof CliLogger.colors &
+    Record<string, ChalkInstance> {
+    return {
+      ...CliLogger.colors,
+      ...this.instanceColors,
+    };
+  }
+
+  private get labelColor(): ChalkInstance {
+    // const label = this.rawLabel.toLocaleLowerCase();
+    const label = this.rawLevel;
+    const key =
+      label in this.colors ? (label as keyof typeof this.colors) : "default";
+    return this.colors[key]!;
+  }
+
+  private get label(): string {
+    if (!this.rawLabel || this.rawLabel.toLowerCase() === "trace") {
+      return "";
+    }
+    return this.labelColor(` ${this.rawLabel} `) + " ";
   }
 
   public get newLine(): string {
@@ -41,8 +113,21 @@ export class CliLogger {
     return this.newLine + this.indent;
   }
 
+  public get scope(): string {
+    if (this.inferScope) {
+      this.rawScope = CliLogger.activeScope;
+    } else if (this.rawScope) {
+      CliLogger.activeScope = this.rawScope;
+    } else {
+      return "";
+    }
+
+    return this.colors.scope(`[${this.rawScope}]`);
+  }
+
   public setScope(scope: string) {
-    this.scope = scope;
+    this.rawScope = scope;
+    CliLogger.activeScope = this.rawScope;
   }
 
   public setLevel(level: LogLevelNames | LogLevelNumber): void {
@@ -53,32 +138,42 @@ export class CliLogger {
     return this.logger.getLevel();
   }
 
+  public log(
+    opts: { level: LogLevelNumber | LogLevelName; label?: string },
+    ...msg: any[]
+  ): void {
+    this.rawLevel = translateLogLevel(opts.level);
+    this.rawLabel = opts.label ?? capitalize(this.rawLevel);
+    if (this.rawLevel === "silent") return;
+    this.logger[this.rawLevel](this.label + this.scope, ...this.replaceIndent(msg));
+  }
+
   public trace(...msg: any[]): void {
-    this.logger.trace(chalk.cyan(this.scope), ...msg);
+    this.log({ level: "trace" }, ...msg);
   }
 
   public debug(...msg: any[]): void {
-    this.logger.debug(chalk.blue(this.scope), ...msg);
+    this.log({ level: "debug" }, ...msg);
   }
 
   public info(...msg: any[]): void {
-    this.logger.info(chalk.green(this.scope), ...msg);
+    this.log({ level: "info" }, ...msg);
   }
 
   public warn(...msg: any[]): void {
-    this.logger.warn(chalk.yellow(this.scope), ...msg);
+    this.log({ level: "warn" }, ...msg);
   }
 
   public error(...msg: any[]): void {
-    this.logger.error(chalk.red(this.scope), ...msg);
+    this.log({ level: "error" }, ...msg);
   }
 
   public var(str: string): string {
-    return chalk.cyan(str);
+    return this.colors.var(str);
   }
 
-  public num(num: number): string {
-    return chalk.cyan(num);
+  public num(num: number | string): string {
+    return this.colors.num(num);
   }
 
   public json(obj: object): string {
@@ -86,6 +181,9 @@ export class CliLogger {
   }
 }
 
-const logger = CliLogger.getInstance();
+export const logger = new CliLogger(undefined, true);
+export const configLogger = new CliLogger("Config");
+export const devLogger = new CliLogger("Dev");
+export const buildLogger = new CliLogger("Build");
 
 export default logger;
